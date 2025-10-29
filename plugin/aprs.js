9c6900c54803a60933a51a1788ca67b3cce8c43e
Henri Bergius <henri.bergius@iki.fi>
2025-10-29 16:37:54 -0500

    Hundreths, not seconds

--- plugin/aprs.js
+++ plugin/aprs.js
@@ -3,7 +3,7 @@ function formatLatitude(coord) {
   const deg = String(Math.floor(degFloat)).padStart(2, '0');
   const minFloat = 60 * (degFloat - Math.floor(degFloat));
   const min = String(Math.floor(minFloat)).padStart(2, '0');
-  const secFloat = 60 * (minFloat - Math.floor(minFloat));
+  const secFloat = 100 * (minFloat - Math.floor(minFloat));
   const sec = String(Math.floor(secFloat)).padStart(2, '0');
   const sign = coord > 0 ? 'N' : 'S';
   return `${deg}${min}.${sec}${sign}`;
@@ -14,7 +14,7 @@ function formatLongitude(coord) {
   const deg = String(Math.floor(degFloat)).padStart(3, '0');
   const minFloat = 60 * (degFloat - Math.floor(degFloat));
   const min = String(Math.floor(minFloat)).padStart(2, '0');
-  const secFloat = 60 * (minFloat - Math.floor(minFloat));
+  const secFloat = 100 * (minFloat - Math.floor(minFloat));
   const sec = String(Math.floor(secFloat)).padStart(2, '0');
   const sign = coord > 0 ? 'E' : 'W';
   return `${deg}${min}.${sec}${sign}`;
